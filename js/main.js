5c4f378a39d1cd560d1410a8abe69da88583e0ea
erinmilling <mill0814@algonquinlive.com>
2016-03-17 12:21:57 -0400

    deleted repetitive code

--- js/main.js
+++ js/main.js
@@ -4,12 +4,8 @@ var $h2 = $('h2');
 var $p = $('p');
 var $li = $('li');
 
-
 grains.forEach(function (grain) {
-  var $h2 = $('<h2>');
   var $img = $('<img>');
-  var $p = $('<p>');
-  var $li = $('<li>');
 
   $img.attr('src', 'images/' + grain.img);
   $h2.html(grain.name);
